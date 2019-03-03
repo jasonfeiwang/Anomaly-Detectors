67ba83dfa6b76affa65499e152ddcf7c7f64ccac
Fei Wang <jason.feiwang@gmail.com>
2019-03-03 15:19:59 -0800

    clean up outlier detection notebook via stl

--- src/r/XmR_Charts.r
+++ src/r/XmR_Charts.r
@@ -5,6 +5,7 @@ library(lubridate)
 library(forecast)
 library(zoo)
 library(gridExtra)
+library(ggQC)
 setwd("/Users/feiwang/Desktop/capstone/anomalyDetectors/src/r")
 tb_all = read_csv("../../output/NYCHA_TS.csv")
 tb_all = select(tb_all, c('Building_Meter', 'Month', 'Imputed_KWH'))
@@ -32,7 +33,9 @@ filter(accounts, na_perc > 0.1)
 account = '1.0 - BLD 01_7836716'
 
 # no missing value
-account = '102.0 - BLD 02_8107087'
+account = '165.0 - BLD 03_90327795'
+  
+'102.0 - BLD 02_8107087'
 
 '101.0 - BLD 04_7421675'
 '164.0 - BLD 01_96973681'
@@ -43,7 +46,7 @@ account = '102.0 - BLD 02_8107087'
 
 
 tb =  select(filter(tb_all, Building_Meter == account), 'Month', 'Imputed_KWH')
-
+tb
 # XmR charts
 
 # plot the two charts in one page
@@ -51,21 +54,45 @@ require(gridExtra)
 
 # XmR on residual part
 ts_anomalized <- tb %>%
-  time_decompose(value.forecasted, method = "STL", merge = TRUE)
+  time_decompose(Imputed_KWH, method = "STL", merge = TRUE)
+ts_anomalized <- rename(ts_anomalized, Residual = remainder)
 
-x_Plot <- ggplot(ts_anomalized, aes(x = month, y = remainder)) + geom_point() + geom_line() + 
+x_Plot <- ggplot(ts_anomalized, aes(x = Month, y = Residual)) + geom_point() + geom_line() + 
   stat_QC(method = 'XmR', auto.label = T, label.digits = 2, show.1n2.sigma = F) 
 
-mR_Plot <- ggplot(ts_anomalized, aes(x = month, y = remainder)) + stat_mR() + stat_QC_labels(method="mR")
+mR_Plot <- ggplot(ts_anomalized, aes(x = Month, y = Residual)) + stat_mR() + stat_QC_labels(method="mR")
 
 grid.arrange(x_Plot, mR_Plot, nrow=2)
 
 
 # summarize the control limits
-QC_Lines(data = ts_anomalized$remainder, method = "XmR")     
+ctrl_limits <- QC_Lines(data = ts_anomalized$Residual, method = "XmR")     
+ts_anomalized <- cbind(ts_anomalized, ctrl_limits)
+
+cal_dev <- function(x) {
+  if (x$Residual < x$xBar_one_LCL) {
+    return(x$xBar_one_LCL - x$Residual)
+  } else if (x$Residual > x$xBar_one_UCL) {
+    return(x$Residual - x$xBar_one_UCL)
+  } else {
+    return (NA)
+  }
+}
+
+dev <- rep(NA, nrow(ts_anomalized))
+for (i in 1:nrow(ts_anomalized)) {
+  dev[[i]] <- cal_dev(ts_anomalized[i, ])
+}
+ts_anomalized$Dev = dev
+
+anomalies <- select(arrange(filter(ts_anomalized, !is.na(dev)), desc(dev)), c('Month', 'Imputed_KWH', 'trend', 'season', 'Residual'))
+rename(anomalies, Observed = Imputed_KWH, Trend = trend, Seasonal = season)
+
+mutate(ts_anomalized, dev = cal_dev(ts_anomalized))
+mutate_all(ts_anomalized, .funs = cal_dev)
 
 
-
+# XmR_Plots on original data:
 x_Plot <- ggplot(tb, aes(x = Month, y = Imputed_KWH)) + geom_point() + geom_line() + 
   stat_QC(method = 'XmR', auto.label = T, label.digits = 2, show.1n2.sigma = F) 
 
@@ -73,3 +100,28 @@ x_Plot
 
 mR_Plot <- ggplot(tb, aes(x = Month, y = Imputed_KWH)) + stat_mR() + stat_QC_labels(method="mR")
 mR_Plot
+
+
+
+map_table=tibble(id=c(1,2,3), 
+                 value=c("a", "b", "c")
+)
+
+map_table
+
+get_label <- function(x) 
+{
+  res=filter(map_table, id==x)$value
+  return(res)
+}
+
+# the data to get the label
+X_data=tibble(v1=c(1,2,3), 
+              v2=c(2,2,2),
+              v3=c(3,2,1)
+)
+
+X_data
+
+tib_res_2=mutate_all(X_data, .funs = get_label)
+tib_res_2
--- src/r/r_testing.R
+++ src/r/r_testing.R
@@ -3,10 +3,13 @@ library(tibbletime)
 library(anomalize)
 library(lubridate)
 library(forecast)
-
+library(zoo)
+library(gridExtra)
+setwd("/Users/feiwang/Desktop/capstone/anomalyDetectors/src/r")
 tb_all = read_csv("../../output/NYCHA_TS.csv")
-tb_all = select(tb, c('Building_Meter', 'Month', 'Imputed_KWH'))
+tb_all = select(tb_all, c('Building_Meter', 'Month', 'Imputed_KWH'))
 
+# replace all zero values with NA
 tb_all <- na_if(tb_all, 0)
 
 accounts = tb_all %>% group_by(Building_Meter) 
@@ -25,8 +28,15 @@ dim(accounts)
 filter(accounts, na_perc > 0.1)
 
 
-
+# lots of missing values
 account = '1.0 - BLD 01_7836716'
+account = '1.0 - BLD 02_7694040'
+
+# no missing value
+account = '165.0 - BLD 03_90327795'
+  
+'102.0 - BLD 02_8107087'
+
 '101.0 - BLD 04_7421675'
 '164.0 - BLD 01_96973681'
 '2.0 - BLD 20_6477762'
@@ -40,6 +50,7 @@ ggplot(tb, aes(x = Month, y = Imputed_KWH)) + geom_point() + geom_line()
 
 
 # Forecast package, tsclean for imputation & outlier detection
+# convert the tibble to time series object
 tb <- tb[order(tb$Month), ]
 ts.orig <- ts(tb$Imputed_KWH, start = c(year(min(tb$Month)), month(min(tb$Month))),, frequency = 12)
 ts.clean <- tsclean(ts.orig)
@@ -58,6 +69,7 @@ colnames(df) <- c('month', 'value.orig', 'value.forecasted')
 df <- mutate(df, value.diff = value.forecasted - value.orig)
 filter(df, value.diff != 0)
 
+# better visualization to compare the observed and cleaned
 ggplot() +
   geom_line(data = df.orig, aes(x = month, y = value), color = "red") +
   geom_point(data = df.orig, aes(x = month, y = value), color = "red") +
@@ -73,42 +85,44 @@ grid.arrange(
   )
 
 
-tb <- as_tibble(df)
+# tb <- as_tibble(df)
 
 
-# XmR charts
-
-# plot the two charts in one page
-require(gridExtra)
-
-# XmR on residual part
-ts_anomalized <- tb %>%
-  time_decompose(value.forecasted, method = "STL", merge = TRUE)
-
-x_Plot <- ggplot(ts_anomalized, aes(x = month, y = remainder)) + geom_point() + geom_line() + 
-  stat_QC(method = 'XmR', auto.label = T, label.digits = 2, show.1n2.sigma = F) 
-
-mR_Plot <- ggplot(ts_anomalized, aes(x = month, y = remainder)) + stat_mR() + stat_QC_labels(method="mR")
-
-grid.arrange(x_Plot, mR_Plot, nrow=2)
-
-
-# summarize the control limits
-QC_Lines(data = ts_anomalized$remainder, method = "XmR")     
-
-
-
-x_Plot <- ggplot(tb, aes(x = Month, y = Imputed_KWH)) + geom_point() + geom_line() + 
-  stat_QC(method = 'XmR', auto.label = T, label.digits = 2, show.1n2.sigma = F) 
-
-x_Plot
-
-mR_Plot <- ggplot(tb, aes(x = Month, y = Imputed_KWH)) + stat_mR() + stat_QC_labels(method="mR")
-mR_Plot
 
 
 
 require(graphics)
 
 plot(stl(ts.clean, "per"))
-plot(stl(ts.clean, s.window = 12, t.window = 50, t.jump = 1))
+plot(stl(ts.clean, s.window = 12, t.window = 51, t.jump = 1))
+ts.stl <- stl(ts.clean, s.window = 13, t.window = 51, t.jump = 1)
+ts.stl <- mstl(ts.clean)
+
+ts.orig %>% autoplot()
+ts.clean %>% autoplot()
+
+
+ts.stl1 <- mstl(ts.orig)
+ts.stl1 %>% autoplot()
+
+ts.stl2 <- mstl(ts.clean)
+ts.stl2 %>% autoplot()
+
+
+plot(seasonal(ts.stl))
+plot(trendcycle(ts.stl))
+
+
+
+tb =  select(filter(tb_all, Building_Meter == account), 'Month', 'Imputed_KWH')
+tb[is.na(tb$Imputed_KWH), 'Imputed_KWH'] <- 0
+
+ts_anomalized <- tb %>%
+  time_decompose(Imputed_KWH, method = "STL", merge = TRUE) %>%
+  anomalize(remainder, method = 'iqr', alpha = 0.05) %>%
+  time_recompose()
+ts_anomalized %>% plot_anomaly_decomposition(alpha_dot = 0.5) +
+  ggtitle("Observed & Residuals") 
+
+match(min(tb$Imputed_KWH), ts.orig)
+ts.orig
